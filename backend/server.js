f38354dd870a88d523cf6368a811c8556b0193d6
Sanjana013 <sanjana17317@gmail.com>
2025-10-29 22:31:55 +0530

    fixed CORS origin

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "*",
+    origin: "https://portfolio-website-ochre-ten-87.vercel.app/",
     methods: ["GET", "POST"],
     // credentials: true,
   })
